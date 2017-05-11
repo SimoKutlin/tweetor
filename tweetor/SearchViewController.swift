93eae6daeb9e7442bba929513a35528e07806623
Simo Kutlin <simo.kutlin@gmail.com>
2017-05-12 00:49:37 +0200

    feat: user can use own location to search tweets

--- tweetor/SearchViewController.swift
+++ tweetor/SearchViewController.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2017 spp. All rights reserved.
 //
 
+import CoreLocation
 import FontAwesome_swift
-import MapKit
 import UIKit
 
 class SearchViewController: UIViewController, UISearchBarDelegate {
@@ -16,14 +16,22 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var distanceSlider: UISlider!
     @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var userLocationButton: UIButton!
     
     var tweets = [Tweet]()
+    
+    let locationManager = CLLocationManager()
     
     var searchLocation: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0) { didSet { print("set to \(searchLocation)") } }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         searchBar.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         
         // Initialize distance label
         sliderValueChanged(distanceSlider)
@@ -71,8 +79,24 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
         }
     }
     
+    // location stuff
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            self.searchLocation = location.coordinate
+            self.userLocationButton.isHighlighted = true
+            // no need for further updating here, also saves battery life #lifehack
+            locationManager.stopUpdatingLocation()
+        }
+    }
     
-    // Segueing stuff goes here
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        let alert = UIAlertController(title: "Ooops", message: "Could not fetch user location. \(error)", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    // segueing stuff goes here
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let identifier = segue.identifier {
             switch identifier {
