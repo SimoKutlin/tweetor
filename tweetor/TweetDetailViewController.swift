d5d7e41857d58195005eaa8db867d9a9da0d24eb
Simo Kutlin <simo.kutlin@gmail.com>
2017-05-15 14:23:19 +0200

    feat: add tweet detail view

--- tweetor/TweetDetailViewController.swift
+++ tweetor/TweetDetailViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class TweetDetailViewController: UIViewController {
     
-    var tweetData: Tweet? { didSet { updateGUI() } }
-
+    // UI Elements
+    @IBOutlet weak var fullnameLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var userImage: UIImageView!
     @IBOutlet weak var tweetTextLabel: UILabel!
+    
+    var tweetData: Tweet? { didSet { updateUI() } }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,9 +26,15 @@ class TweetDetailViewController: UIViewController {
     }
 
     
-    private func updateGUI() {
-        if let data = tweetData {
+    private func updateUI() {
+        if let data = tweetData, let user = data.user {
+            print("showing \(tweetData!)")
+            fullnameLabel?.text = user.name
+            usernameLabel?.text = "@" + user.username
+            
             tweetTextLabel?.text = data.text
+            userImage?.image = UIImage(named: user.imageURL)
+            
         }
     }
     
