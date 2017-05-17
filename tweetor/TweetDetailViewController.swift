cb6aa7fbb4133e5da9a63a94b18299f9d585fe1a
Simo Kutlin <simo.kutlin@gmail.com>
2017-05-18 01:17:43 +0200

    feat: show attached photos and videos in tweet detail view

--- tweetor/TweetDetailViewController.swift
+++ tweetor/TweetDetailViewController.swift
@@ -14,10 +14,10 @@ class TweetDetailViewController: UIViewController, UIGestureRecognizerDelegate {
     @IBOutlet weak var fullnameLabel: UILabel!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var timestampLabel: UILabel!
     @IBOutlet weak var tweetTextLabel: UILabel!
     
     var tweetData: Tweet? { didSet { updateUI() } }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,16 +32,47 @@ class TweetDetailViewController: UIViewController, UIGestureRecognizerDelegate {
     private func updateUI() {
         if let tweet = tweetData, let user = tweet.user {
             
-            
             DispatchQueue.global().async {
-                let data = try? Data(contentsOf: URL(string: user.imageURL)!)
+                let thumbnailData = try? Data(contentsOf: URL(string: user.imageURL)!)
+                var imageData: Data? = Data()
+                
+                
+                if tweet.mediaType == "photo" {
+                    imageData = try? Data(contentsOf: URL(string: tweet.mediaURL)!)
+                }
                 
                 DispatchQueue.main.async {
-                    self.userImage?.image = UIImage(data: data!)
+                    self.userImage?.image = UIImage(data: thumbnailData!)
                     print("setting!")
                     self.fullnameLabel?.text = user.name
                     self.usernameLabel?.text = "@" + user.username
+                    self.timestampLabel?.text = tweet.timestamp
                     self.tweetTextLabel?.text = tweet.text
+                    
+                    if tweet.mediaType != "none" {
+                        let frameHeight = self.view.frame.size.height
+                        let frameWidth = self.view.frame.size.width
+                        let horizontalMargins = frameWidth / 20
+                        let viewFrame = CGRect(x: horizontalMargins, y: frameHeight / 3, width: frameWidth - 2 * horizontalMargins, height: frameHeight / 2)
+                        
+                        if tweet.mediaType == "photo" && imageData != nil {
+                            let mediaView = UIImageView()
+                            mediaView.image = UIImage(data: imageData!)
+                            mediaView.frame = viewFrame
+                            mediaView.contentMode = .scaleAspectFit
+                            self.view.addSubview(mediaView)
+                        }
+                        
+                        if tweet.mediaType == "video" {
+                            let mediaView = UIWebView()
+                            mediaView.loadRequest(URLRequest(url: URL(string: tweet.mediaURL)!))
+                            mediaView.frame = viewFrame
+                            mediaView.contentMode = .scaleAspectFit
+                            self.view.addSubview(mediaView)
+                        }
+                        
+                    
+                    }
                 }
             }
         }
